eb40460d091276f8b5b7d44e249f051ad48eb0d7
Oleksii89 <108924918+Oleksii89@users.noreply.github.com>
2023-11-19 17:48:48 +0200

    update

--- src/1.ts
+++ src/1.ts
@@ -1,11 +1,11 @@
-// /*
-//   Клас Student, який містить три властивості: name, age та grade.
-//   Замість того, щоб оголошувати ці властивості в тілі класу, потім у конструкторі, і нарешті надавати їм значення,
-//   напишіть скорочену ініціалізацію.
-// */
+/*
+  Клас Student, який містить три властивості: name, age та grade.
+  Замість того, щоб оголошувати ці властивості в тілі класу, потім у конструкторі, і нарешті надавати їм значення,
+  напишіть скорочену ініціалізацію.
+*/
 
-// class Student {
-//   constructor(public name: string, public age: number, public grade: string) {}
-// }
+class Student {
+  constructor(public name: string, public age: number, public grade: string) {}
+}
 
-// export {};
+export {};
--- src/2.ts
+++ src/2.ts
@@ -15,20 +15,11 @@
 */
 
 class Employee {
-  // Заповніть модифікатори доступу
-  // name: string;
-  // department: string;
-  // salary: number;
-
   constructor(
     public name: string,
     private department: string,
     protected salary: number
-  ) {
-    // this.name = name;
-    // this.department = department;
-    // this.salary = salary;
-  }
+  ) {}
 
   getEmployeeDetails() {
     return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`;
@@ -39,8 +30,5 @@ class Manager extends Employee {
   // Реалізуйте конструктор та збільшіть salary на 10000
   salary = this.salary + 1000;
 }
-
-// const manager = new Manager("alex", "as;fksdf", 0);
-// console.log(manager);
 
 export {};
