c451680de1583ec26326067345ed6b1d90ff0212
Oleksii89 <108924918+Oleksii89@users.noreply.github.com>
2023-11-19 16:58:14 +0200

    update

--- src/4.ts
+++ src/4.ts
@@ -1,10 +1,46 @@
-// const key = new Key();
+class Key {
+  constructor(private signature: number = Math.random()) {}
 
-// const house = new MyHouse(key);
-// const person = new Person(key);
+  getSignature(): number {
+    return this.signature;
+  }
+}
 
-// house.openDoor(person.getKey());
+class Person {
+  constructor(private key: Key) {}
+  getKey(): Key {
+    return this.key;
+  }
+}
 
-// house.comeIn(person);
+abstract class House {
+  constructor(protected key: Key) {}
+  protected door: boolean;
+  private tenants: Person[] = [];
 
-// export {};
+  comeIn(person: Person) {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+  public abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key) {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+
+const key = new Key();
+
+const house = new MyHouse(key);
+const person = new Person(key);
+
+house.openDoor(person.getKey());
+
+house.comeIn(person);
+
+export {};
